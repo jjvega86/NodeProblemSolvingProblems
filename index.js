07f1affb81d35a1d5a4afeda5510fee278710502
JJ <jj.vega86@gmail.com>
2022-08-26 08:31:20 -0400

    feat: added DivisibleSumPairs docstring, function skeleton, and two test cases

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 //TODO: Write tests for MilitaryTimeConverter
 //TODO: Write tests for MinMaxSum
 //TODO: Write tests for TwoSum
-
-const processData = require("./HackerRankProblems/CamelCaseFour/CamelCaseFour");
-
-console.log(processData("S;M;plasticCup()"));
