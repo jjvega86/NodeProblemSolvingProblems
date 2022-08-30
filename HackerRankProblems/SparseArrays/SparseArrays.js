623849a9d514429b4d93dfdd31224269a69e4296
JJ <jj.vega86@gmail.com>
2022-08-30 09:05:01 -0400

    Finished and tested matchingStrings function

--- HackerRankProblems/SparseArrays/SparseArrays.js
+++ HackerRankProblems/SparseArrays/SparseArrays.js
@@ -3,13 +3,23 @@
  For each query string, determine how many times it occurs in the list of input strings. 
  Return an array of the results.
 
+ Big O - O(n^2) due to nested forEach loops. Steps will increase exponentially with input size.
  * @param {string[]} strings an Array of strings to search
  * @param {string[]} queries an Array of strings to query with
  * @returns {number[]} an Array of numbers counting substring matches between queries and strings
  */
 function matchingStrings(strings, queries) {
   let matches = [];
-  // Write your code here
+  let currentQueryIndex = 0;
+  queries.forEach((query) => {
+    matches[currentQueryIndex] = 0;
+    strings.forEach((string) => {
+      if (query === string) {
+        matches[currentQueryIndex]++;
+      }
+    });
+    currentQueryIndex++;
+  });
   return matches;
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 //TODO: Write tests for MilitaryTimeConverter
 //TODO: Write tests for MinMaxSum
 //TODO: Write tests for TwoSum
+
+const matchingStrings = require("./HackerRankProblems/SparseArrays/SparseArrays");
+
+console.log(
+  matchingStrings(["aba", "baba", "aba", "xzxb"], ["aba", "xzxb", "ab"])
+);
