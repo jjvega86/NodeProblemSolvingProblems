0a9c0d716933eb62305162c032e7940944c25001
JJ <jj.vega86@gmail.com>
2022-09-07 08:44:17 -0400

    feat: added GradingStudents boilerplate and tests

---
+++ HackerRankProblems/GradingStudents/GradingStudents.js
@@ -0,0 +1,16 @@
+/**
+ * Given the initial value of grade for each of a teacher's students
+ * write code to automate a rounding process
+ * Rules:
+ *      If the difference between grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5
+ *      If the value of grade is less than 40, no rounding occurs (failing grade)
+ * @param {number[]} grades Student grades before rounding
+ * @returns {number[]} grades after rounding
+ */
+function gradingStudents(grades) {
+  let roundedGrades = [];
+
+  return roundedGrades;
+}
+
+module.exports = gradingStudents;
---
+++ HackerRankProblems/GradingStudents/GradingStudents.test.js
@@ -0,0 +1,13 @@
+const gradingStudents = require("./GradingStudents");
+
+test("Grades 84, 29, 57 become 85, 29, 57", () => {
+  expect(gradingStudents([84, 29, 57])).toEqual([85, 29, 57]);
+});
+
+test("Grades 82, 29, 58 become 82, 29, 60", () => {
+  expect(gradingStudents([82, 29, 58])).toEqual([82, 29, 60]);
+});
+
+test("Grades 78, 41, 37 become 80, 41, 37", () => {
+  expect(gradingStudents([78, 41, 37])).toEqual([80, 41, 37]);
+});
