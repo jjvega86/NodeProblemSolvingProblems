d7bf51059c6a71573a955c6b1c6a3218a043b5de
JJ <jj.vega86@gmail.com>
2022-09-13 06:44:41 -0400

    feat: added boilerplate, JSDoc, and tests for FlippingBits

---
+++ HackerRankProblems/FlippingBits/FlippingBits.js
@@ -0,0 +1,15 @@
+/**
+ * Receive an integer
+ * Convert integer to bits
+ * Flip each bit to its opposite (0 -> 1, 1 -> 0)
+ * Convert back to unsigned integer and return
+ * @param {number} n integer
+ * @returns {number} unsigned integer
+ */
+function flippingBits(n) {
+  let finalResult;
+
+  return finalResult;
+}
+
+module.exports = flippingBits;
--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-//TODO: Write tests for MilitaryTimeConverter
-//TODO: Write tests for MinMaxSum
-//TODO: Write tests for TwoSum
 
-
-const gradingStudents = require("./HackerRankProblems/GradingStudents/GradingStudents");
-
-gradingStudents([73, 67, 38, 33]);
 
