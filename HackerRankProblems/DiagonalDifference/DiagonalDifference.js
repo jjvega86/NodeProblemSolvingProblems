b905e9825eb35553929ccf8ced5f0d4bd3a21fe2
JJ <jj.vega86@gmail.com>
2022-09-19 06:51:01 -0400

    👌 IMPROVE: Complete solution; tests pass.

--- HackerRankProblems/DiagonalDifference/DiagonalDifference.js
+++ HackerRankProblems/DiagonalDifference/DiagonalDifference.js
@@ -1,5 +1,14 @@
 function diagonalDifference(arr) {
-  // Write your code here
+  let firstSum = 0;
+  let secondSum = 0;
+  let len = arr.length - 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    firstSum += arr[i][i];
+    secondSum += arr[i][len - i];
+  }
+
+  return Math.abs(firstSum - secondSum);
 }
 
 module.exports = diagonalDifference;
